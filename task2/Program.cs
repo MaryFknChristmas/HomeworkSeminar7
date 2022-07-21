c229d9295ea1ac002541c79af92cbfbce7512578
Marina <gtellier.mecanics@gmail.com>
2022-07-21 17:15:34 +0300

    Значение элемента  по индексу fixed

--- task2/Program.cs
+++ task2/Program.cs
@@ -24,14 +24,11 @@ Console.WriteLine("Введите номер строки");
 int userRow = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите номер столбца");
 int userColumn = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            if (i == userRow-1 && j == userColumn-1)
-            {
-                Console.WriteLine("Ваше число: " + array[i, j]);
-            }
-        }
-    }
+if (userRow >= 1 && userRow <= rows && userColumn >= 1 && userColumn <= columns)
+{
+    Console.WriteLine("Ваше число: " + array[userRow-1, userColumn-1]);
+}
+else
+{
+    Console.WriteLine("Числа не существует");
+}
