f931ba9a3e4bcf153fe726c8e9857fa7f4229b48
Marina <gtellier.mecanics@gmail.com>
2022-07-21 16:26:56 +0300

    Значение элемента  по индексу

---
+++ task2/Program.cs
@@ -0,0 +1,37 @@
+﻿Random random = new Random();
+int rows = 3;
+int columns = 4;
+int[,] array = new int[rows, columns];
+
+for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(0, 11);
+        }
+    }
+
+for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+Console.WriteLine("Введите номер строки");
+int userRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int userColumn = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (i == userRow-1 && j == userColumn-1)
+            {
+                Console.WriteLine("Ваше число: " + array[i, j]);
+            }
+        }
+    }
