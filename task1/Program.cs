8a99e3effb6e367a35f70c0826a2f427dbfc72ad
Marina <gtellier.mecanics@gmail.com>
2022-07-21 14:03:46 +0300

    Двумерный массив с вещественными числами

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Random random = new Random();
+int rows = 3;
+int columns = 4;
+double[,] array = new double[rows, columns];
+
+for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.NextDouble() * 20 - 10;
+        }
+    }
+
+for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(Math.Round(array[i, j], 1) + "\t");
+        }
+        Console.WriteLine();
+    }
